b682d926fa39b38aac86e4f9393e734208d61a11
student <student@S006-2NQ185J-U>
2017-10-16 13:23:19 +0200

    poczatek

---
+++ zajecia_1/labirynt/__init__.py
@@ -0,0 +1,19 @@
+plik = open("labirynt0.txt")
+znak = plik.readlines()
+dlugosc = int(znak[0][0:2])
+szerokosc = int(znak[0][3:5])
+#print dlugosc 
+#print szerokosc
+poczatek = "@"
+koniec = "$"
+
+for i in xrange(1,dlugosc+1):
+    for x in xrange(szerokosc):
+        print znak[i][x]
+        if znak[i][x]==poczatek:
+            print "znalazlem"
+            if x+1<=szerokosc:
+                if znak[i][x+1]=='#':
+                    if znak[i][x-1]>=szerokosc:
+                    if znak[i][x-1]
+            
